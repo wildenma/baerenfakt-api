7c7cd252497608f9ecc12cc6b912d7034a96d5fe
wildenma <77071102+wildenma@users.noreply.github.com>
2025-08-07 10:01:30 +0200

    Update random.js

--- api/random.js
+++ api/random.js
@@ -1,10 +1,14 @@
-const fs = require("fs");
-const path = require("path");
+import { promises as fs } from "fs";
+import path from "path";
 
-export default function handler(req, res) {
-  const filePath = path.resolve(process.cwd(), "baerenfakten.txt");
-  const fileContent = fs.readFileSync(filePath, "utf-8");
-  const facts = fileContent.split("\n").filter(Boolean);
-  const randomFact = facts[Math.floor(Math.random() * facts.length)];
-  res.status(200).send(randomFact);
+export default async function handler(req, res) {
+  const filePath = path.join(process.cwd(), "baerenfakten.txt");
+  try {
+    const fileContent = await fs.readFile(filePath, "utf-8");
+    const facts = fileContent.split("\n").filter(Boolean);
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    res.status(200).send(randomFact);
+  } catch (error) {
+    res.status(500).send("Fehler beim Lesen der Datei");
+  }
 }
